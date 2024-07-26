b4578459daff7022b5941f1bee1d16ffc8b0d4ac
Mr kuttusan <yedhukrishnanu2001@gmail.com>
2024-07-26 16:45:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -99,7 +99,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 VERIFY = bool(environ.get('VERIFY', True))
 VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'publicearn.com')
-VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '')
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'de08290b12d9e34191f3a057070a50a18187fff3')
 # if verify second shortner is True then fill below url and api
 VERIFY_SND_SHORTLINK_URL = environ.get('VERIFY_SND_SHORTLINK_URL', '')
 VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
